f0ee60328a9af3bcf7d317db49ce10ebb194783d
Martin Halawa <martinhalawa12345@gmail.com>
2023-12-12 21:51:47 +0700

    recom space2

--- find_house_app/lib/widgets/city_card.dart
+++ find_house_app/lib/widgets/city_card.dart
@@ -2,7 +2,6 @@ import "package:find_house_app/models/city.dart";
 import "package:find_house_app/theme.dart";
 import "package:flutter/material.dart";
 
-
 class CityCard extends StatelessWidget {
   final City city;
 
@@ -13,52 +12,54 @@ class CityCard extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(18),
       child: Container(
-      height: 160,
-      width: 120,
-      color: Color(0xffF6F7F8),
-      child: Column(
-        children: [
-          Stack(
+          height: 160,
+          width: 120,
+          color: Color(0xffF6F7F8),
+          child: Column(
             children: [
-              Image.asset(
-                city.imageUrl,
-                width: 130,
-                height: 102,
-                fit: BoxFit.cover,
-               ),
-               city.isPopular ? Align(
-                alignment: Alignment.topRight,
-                  child: Container(
-                  width: 50,
-                  height: 28,
-                  decoration: BoxDecoration(
-                    color: purpleColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                    ),
+              Stack(
+                children: [
+                  Image.asset(
+                    city.imageUrl,
+                    width: 130,
+                    height: 102,
+                    fit: BoxFit.cover,
                   ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/images/star_icon.png',
-                      width: 22,
-                      height: 22,
-                      ),
-                  ),
-                 ),
-               ) : Container(),
+                  city.isPopular
+                      ? Align(
+                          alignment: Alignment.topRight,
+                          child: Container(
+                            width: 50,
+                            height: 28,
+                            decoration: BoxDecoration(
+                              color: purpleColor,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(30),
+                              ),
+                            ),
+                            child: Center(
+                              child: Image.asset(
+                                'assets/images/star_icon.png',
+                                width: 22,
+                                height: 22,
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
+              ),
+              SizedBox(
+                height: 11,
+              ),
+              Text(
+                city.name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
             ],
-          ),        
-           SizedBox(height: 11,
-           ),
-           Text(
-            city.name,
-            style: blackTextStyle.copyWith(
-              fontSize: 16,
-            ),
-            ),
-          ],
-      )
-      ),
+          )),
     );
   }
 }
--- find_house_app/lib/widgets/space_card.dart
+++ find_house_app/lib/widgets/space_card.dart
@@ -1,22 +1,93 @@
+import 'package:find_house_app/theme.dart';
 import 'package:flutter/material.dart';
 
-
 class SpaceCard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return Row(
       children: [
-        Container(
-          width: 130,
-          height: 110,
-          child: Stack(
-            children: [
-              Image.asset(
-                'assets/images/space1.png',
-              ),
-            ],
+        ClipRRect(
+          borderRadius: BorderRadius.circular(18),
+          child: Container(
+            width: 130,
+            height: 106,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/space1.png',
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 70,
+                    height: 28,
+                    decoration: BoxDecoration(
+                      color: purpleColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/star_icon.png',
+                          width: 22,
+                          height: 22,
+                        ),
+                        Text(
+                          '4/5',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Rumah Velman',
+              style: blackTextStyle.copyWith(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text.rich(
+              TextSpan(
+                  text: '2000k',
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: '/month',
+                      style: greyTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ]),
+            ), 
+            SizedBox(
+              height: 14,
+            ),
+            Text(
+              'Bojongsoang, Bandung', 
+              style: greyTextStyle,
+            ),
+          ],
         ),
       ],
     );
