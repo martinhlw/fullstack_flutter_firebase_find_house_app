589cf186b2915c00bca47deafc3ffc493c547382
Martin Halawa <martinhalawa12345@gmail.com>
2023-12-13 23:21:56 +0700

    Finishing Home & Details Page

--- find_house_app/lib/pages/detail_page.dart
+++ find_house_app/lib/pages/detail_page.dart
@@ -1,10 +1,12 @@
 import "package:find_house_app/theme.dart";
+import "package:find_house_app/widgets/facility_item.dart";
 import "package:flutter/material.dart";
 
 class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whiteColor,
       body: SafeArea(
         bottom: false,
         child: Stack(
@@ -14,30 +16,6 @@ class DetailPage extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               height: 400,
               fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 25,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      'assets/images/btn_back.png',
-                      width: 40,
-                    ),
-                  ),
-                  Image.asset(
-                    'assets/images/btn_wishlist.png',
-                    width: 40,
-                  ),
-                ],
-              ),
             ),
             ListView(
               children: [
@@ -52,6 +30,7 @@ class DetailPage extends StatelessWidget {
                     color: whiteColor,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(
                         height: 30,
@@ -97,45 +76,219 @@ class DetailPage extends StatelessWidget {
                                 Image.asset(
                                   'assets/images/star_icon.png',
                                   width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
                                   'assets/images/star_icon.png',
                                   width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
                                   'assets/images/star_icon.png',
                                   width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
                                   'assets/images/star_icon.png',
                                   width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
-                                  'assets/images/star_icon.png',
-                                  width: 20,
-                                  color: Color(0xff989BA1)
-                                  ),            
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset('assets/images/star_icon.png',
+                                    width: 20, color: Color(0xff989BA1)),
                               ],
                             ),
                           ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Facilities',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'kitchen',
+                              imageUrl: 'assets/images/icon_kitchen.png',
+                              total: 1,
+                            ),
+                            FacilityItem(
+                              name: 'bedroom',
+                              imageUrl: 'assets/images/icon_bedroom.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name: 'bathroom',
+                              imageUrl: 'assets/images/icon_bathroom.png',
+                              total: 3,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE: PHOTOS
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Photos',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        height: 92,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            SizedBox(
+                              width: edge,
+                            ),
+                            Image.asset(
+                              'assets/images/photo1.png',
+                              width: 115,
+                              height: 92,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 22,
+                            ),
+                            Image.asset(
+                              'assets/images/photo2.png',
+                              width: 115,
+                              height: 92,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 22,
+                            ),
+                            Image.asset(
+                              'assets/images/photo3.png',
+                              width: 115,
+                              height: 92,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: edge,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE : LOCATION
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Location',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Jln. Ciganitri No. C28\nBandung',
+                              style: greyTextStyle,
+                            ),
+                            Image.asset(
+                              'assets/images/btn_map.png',
+                              width: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - (2 * edge),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary:
+                                purpleColor, // Menggunakan primary untuk menentukan warna
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(17),
+                            ),
+                          ),
+                          child: Text(
+                            'Book Now',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 18,
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 25,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/images/btn_back.png',
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/btn_wishlist.png',
+                    width: 40,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
